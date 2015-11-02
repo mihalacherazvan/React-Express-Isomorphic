4eaddb65d1479cc54012d7150d5c2b225890d174
mihalacherazvan <mrazvani@yahoo.com>
2015-11-02 12:30:21 +0200

    Used react-dom for rendering

--- components/ReactApp.js
+++ components/ReactApp.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 export default class ReactApp extends React.Component {
-	constructor(props) {
+	constructor (props) {
 		super(props);
 	}
-	render() {
+	
+	render () {
 		return (
 			<div>Hello, Dave! You're looking well today.</div>
 		);
