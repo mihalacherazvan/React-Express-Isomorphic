90419beb62db95714c4a18610ab303dcc6b332a4
mihalacherazvan <mrazvani@yahoo.com>
2015-12-24 09:15:48 -0500

    renames app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ReactApp from './build/ReactApp.min.js';
-
-window.onload = function() {
-	ReactDOM.render( <ReactApp/>, document.getElementById('react-app') );
-};
