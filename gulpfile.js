474d75fa50af84744446773670724d0e4d2ffe70
mihalacherazvan <mrazvani@yahoo.com>
2015-12-28 04:21:06 -0500

    removed gulp-nodemon in favor of basic node process killing/spawning

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,36 @@ var gulp  	= require('gulp'),
 	plugins = require('gulp-load-plugins')({ camelize: true }),
     browserify = require('browserify'),
     buffer = require('vinyl-buffer'),
-    source = require('vinyl-source-stream');
+    source = require('vinyl-source-stream'),
+    spawn = require('child_process').spawn,
+    node;
 
-var server_build_count = 0;
-var react_app_build_count = 0;
+var client_render_count = 1;
 
 var paths = {
-    server      : ['./index.js'],
-    client      : ['./client-render.js', './components/**/*.js'],
+    client : ['./client-render.js', './components/**/*.js'],
 };
 
-gulp.task('running/restarting server',  function () {
-    plugins.nodemon({
-        script: './index.js',
-        ext: 'js html', 
-        env: { 'NODE_ENV': 'development' }
-    })
+gulp.task('running/restarting server', ['building react client render'], function () {
+    if (node) 
+    { 
+        node.kill();
+    }
+
+    node = spawn('node', ['index.js'], {stdio: 'inherit'});
+
+    node.on('close', function (code) {
+        if (code === 8) 
+        {
+            gulp.log('Error detected, waiting for changes...');
+        }
+    });
 });
 
+
+
 gulp.task('building react client render', function () {
+    console.log('Client render build ', client_render_count++);
     return browserify({
             entries: './client-render.js'
         }).
@@ -35,8 +46,7 @@ gulp.task('building react client render', function () {
 });
 
 gulp.task('watching', function() {
-	//gulp.watch( paths.server, ['building server'] );
-    gulp.watch( paths.client, ['building react client render'] );
+    gulp.watch( paths.client, ['building react client render', 'running/restarting server'] );
 });
 
 gulp.task('default', ['building react client render', 'watching', 'running/restarting server'] );
